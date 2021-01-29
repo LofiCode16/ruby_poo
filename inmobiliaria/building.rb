2b7c0b70d4f28d6847a01387f09657c0fea21978
Vicente Carmona <carmonavn@gmail.com>
2021-01-29 16:09:27 -0300

    Coty, Deyna, Tomas y Vicente

--- inmobiliaria/building.rb
+++ inmobiliaria/building.rb
@@ -1,15 +1,74 @@
+require_relative "office"
+
 class Building
+    include Office
+    attr_accessor :nombre, :realState, :construction, :venta, :admin
+    
+    def initialize (nombre, realState, construction)
+        @nombre = nombre
+        @realState = realState
+        @construction = construction
+        @venta = Sales::Sales.new("Oficina Venta #{self.nombre}")
+        @admin = Admin::Admin.new("Oficina Administración #{self.nombre}")
+    end
 end
 
-class Construction
+
+
+class Construction #constructora
+    include Office
+    attr_accessor :nombre, :admins
+
+    def initialize (nombre)
+        @nombre = nombre
+        @admins = []
+        4.times do |i|
+            @admins.push(Admin::Admin.new("Oficina de Administración #{i+1}"))
+        end
+    end
+
 end
 
-class RealState
+class RealState #inmo
+    include Office
+    attr_accessor :nombre, :admin, :ventas
+   
+    def initialize (nombre)
+        @nombre = nombre
+        @admin = Admin::Admin.new("Oficina de administración #{self.nombre}")
+        @ventas = []
+        3.times do |i|
+            @ventas.push(Sales::Sales.new("Oficina de venta #{i+1}"))
+        end
+    end
+
 end
 
-# 1.- construir 3 edificios
-# 2.- cada edificio pertenece a una inmobiliaria
-# 3.- los edificios son hechos por la misma constructora
-# 4.- cada edificio tendrá una oficina administrativa y una de ventas
-# 5.- la inmobiliaria tendrá una oficina administrativa y 3 de ventas
-# 6.- la constructora tendrá 4 oficinas administrativas
+
+constru = Construction.new ('Constructora 1')
+
+inmo1 = RealState.new('Inmobiliaria  1')
+inmo2 = RealState.new('Inmobiliaria 2')
+inmo3= RealState.new('Inmobiliaria 3')
+
+edificio1 = Building.new('edificio 1', inmo1, constru)
+edificio2 = Building.new('edificio 2', inmo2, constru)
+edificio3 = Building.new('edificio 3', inmo3, constru)
+
+
+puts constru.nombre
+puts inmo1.nombre
+puts edificio1.nombre
+
+constru.admins.each do |i|
+    puts i.name
+end
+
+inmo1.ventas.each do |i|
+    puts i.name
+end
+
+puts edificio1.nombre
+puts edificio1.construction.nombre
+puts edificio1.realState.nombre
+
--- inmobiliaria/office.rb
+++ inmobiliaria/office.rb
@@ -1,7 +1,19 @@
 module Office
     module Sales
+        class Sales
+            attr_accessor :name
+            def initialize(name)
+                @name = name
+            end
+        end
     end
 
     module Admin
+        class Admin
+            attr_accessor :name
+            def initialize(name)
+                @name = name
+            end
+        end
     end
 end
